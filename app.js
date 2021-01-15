a5995ae69a318e6b0a52a02af3b428acbe7a260f
breakfastitem <andrew.ehrman@gmail.com>
2021-01-15 01:33:15 -0500

    established connection with database

--- app.js
+++ app.js
@@ -2,6 +2,24 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require("console.table");
+const dotenv =require("dotenv");
 
 //Configure env file
 dotenv.config();
+
+//Establish mysql connection to database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "employees_db"
+});
+
+connection.query("SELECT * FROM department",(err,response)=>{
+    if(err) throw err;
+
+    console.table(response);
+
+    connection.end();
+});
