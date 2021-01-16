a51b1510c3e76359a6d4027aec84063e7d31c41d
breakfastitem <andrew.ehrman@gmail.com>
2021-01-16 01:29:40 -0500

    Added and implemented the view all employees option

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require("console.table");
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
 
 //Configure env file
 dotenv.config();
@@ -16,10 +16,47 @@ const connection = mysql.createConnection({
     database: "employees_db"
 });
 
-connection.query("SELECT * FROM department",(err,response)=>{
-    if(err) throw err;
+//Initial menu prompt
+inquirer.prompt([{
+    type: "list",
+    message: "What Would You Like to Do.",
+    choices: ["View All Employees"],
+    name: "funct"
+}]).then((answers) => {
 
-    console.table(response);
+    switch (answers.funct) {
+        case "View All Employees":
+           connection.query(`
+                SELECT 
+                    *
+                FROM role
 
-    connection.end();
-});
+                RIGHT JOIN employee ON employee.role_id= role.id
+                INNER JOIN department ON role.department_id=department.id;`, (err, response) => {
+                if (err) throw err;
+                let table = [];
+                for (let i = 0; i < response.length; i++) {
+                    let employee = { id: i + 1 };
+                    employee.first_name = response[i].first_name;
+                    employee.last_name = response[i].last_name;
+                    employee.title = response[i].title;
+                    employee.department = response[i].name;
+                    if (response[i].manager_id) {
+                        employee.manager = response[response[i].manager_id - 1].first_name + " " + response[response[i].manager_id - 1].last_name;
+                    } else {
+                        employee.manager = "NULL";
+                    }
+                    table.push(employee);
+                }
+                console.table(table);
+
+            });
+            break;
+    }
+
+
+
+
+}
+
+);
